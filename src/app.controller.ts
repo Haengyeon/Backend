5c99cb5b88755bb0c0207893b64aa1f2c5291b0b
ssojungg <kawy.sojung@gmail.com>
2026-08-31 02:04:01 +0900

    fix: 스웨거수정

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeController } from '@nestjs/swagger';
 import { AppService } from './app.service';
 
+// 서버가 떠 있는지 확인하는 용도라 스웨거 문서에는 넣지 않는다
+@ApiExcludeController()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
